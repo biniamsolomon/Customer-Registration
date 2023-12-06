644d16734943e3c6a162b365f0fd726dc08a96f7
biniam.solomon <biniam.solomon@safaricom.et>
2023-12-06 14:14:55 +0300

    first commit

---
+++ gitlabRepository/src/main/java/com/biniscode/gitlabRepository/GitlabRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.biniscode.gitlabRepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabRepositoryApplication.class, args);
+	}
+
+}
---
+++ gitlabRepository/src/test/java/com/biniscode/gitlabRepository/GitlabRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biniscode.gitlabRepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitlabRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
